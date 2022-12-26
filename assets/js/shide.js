ee6dd3e1f2b99886b32c7054ad929432bee8fcb1
nhan405 <trinhan21420@gmail.com>
2022-12-26 23:41:21 +0700

    first commit

---
+++ assets/js/shide.js
@@ -0,0 +1,15 @@
+/* js cho phần shide chuyển động*/
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
